d0633074896cb039faf3dd9d80218791f8b73c8b
noah <nbaertsch@github.com>
2021-12-04 19:36:11 -0500

    Just pushing for funsies

---
+++ fundamentals.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// var s1 string = "string1"
+	// s2 := "string2"
+
+	count := 3
+
+	for i := 0; i < count; i++ {
+		if i%2 == 0 {
+			print(i, " is an even number.")
+		} else {
+			print(i, " is an odd number.")
+		}
+	}
+}
---
+++ shell-exec/syscall.go
@@ -0,0 +1,21 @@
+// I do not know what the fuck I am doing here
+package main
+
+func main() {
+	// var s1 string = "string1"
+	// s2 := "string2"
+
+	count := 3
+
+	for i := 0; i < count; i++ {
+		if i%2 == 0 {
+			print(i, " is an even number.")
+		} else {
+			print(i, " is an odd number.")
+		}
+	}
+}
+
+func method_Syscall(sc []byte) {
+
+}
